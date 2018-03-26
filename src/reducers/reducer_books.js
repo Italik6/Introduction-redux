db7e027be57dc94827f3752a748ae6a4e081c841
Michal Turek <michael.turek@rizk.com>
2018-03-26 10:43:50 +0200

    book-list-container

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+export default function () {
+    return [
+        {title: 'JavaScript' },
+        {title: 'Jacke Kaczmarksi' },
+        {title: 'Autobiography' },
+        {title: 'Tolkien' }
+    ]
+}
